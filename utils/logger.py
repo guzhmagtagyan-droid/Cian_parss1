808731bfc309d6f703e451a09a1e69bc75d119eb
guzhmagtagyan-droid <guzhmagtagyan@gmail.com>
2026-03-04 12:02:52 +0300

    Add logging setup function

---
+++ utils/logger.py
@@ -0,0 +1,25 @@
+import logging
+
+def setup_logging(log_file='app.log'):
+    # Create a logger
+    logger = logging.getLogger()
+    logger.setLevel(logging.DEBUG)
+
+    # Create a file handler
+    file_handler = logging.FileHandler(log_file)
+    file_handler.setLevel(logging.DEBUG)
+
+    # Create a console handler
+    console_handler = logging.StreamHandler()
+    console_handler.setLevel(logging.ERROR)
+
+    # Create a formatter and set it for both handlers
+    formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+    file_handler.setFormatter(formatter)
+    console_handler.setFormatter(formatter)
+
+    # Add the handlers to the logger
+    logger.addHandler(file_handler)
+    logger.addHandler(console_handler)
+
+    return logger
